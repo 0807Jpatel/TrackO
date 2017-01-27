44abec99f71dcabc40e6ab20760440c7169d092e
Jay Patel <0807Jpatel@gmail.com>
2017-01-27 17:42:57 -0500

    more error fixing

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function sendText(sender, text){
         qs: {access_token: token},
         method: "POST",
         json: {
-            recipient: {id, sender},
+            recipient: {id: sender},
             message: messageData
         }
     }, function(error, response, body){
