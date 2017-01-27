8f3872be575fd23d56db61483e0d736a89b63952
Jay Patel <0807Jpatel@gmail.com>
2017-01-27 15:03:40 -0500

    added () to the express

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const app = express;
-
+const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: false}));
