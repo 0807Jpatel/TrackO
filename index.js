45cec853ad7c5a9f888159ab8cdf6cfa224986dd
Jay Patel <0807Jpatel@gmail.com>
2017-01-28 00:46:35 -0500

    more error fixing

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const shippo = require('shippo');
 
 const app = express();
 app.set('port', (process.env.PORT || 5000));
