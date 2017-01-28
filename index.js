9ed93d78b88251934c988bea41a9fed009dace2e
Jay Patel <0807Jpatel@gmail.com>
2017-01-28 09:59:41 -0500

    more error fixing

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get('/packageUpdate/', function(req, res){
 
 app.post('/packageUpdate/', function(req, res){
     sendText( 1386905277995957 , "something updated");
-    console.log(res.track_updated + "THIS IS WHERE I PRINTED OUT ");
+    console.log(JSON.stringify(req));
     res.send(200);
 })
 
