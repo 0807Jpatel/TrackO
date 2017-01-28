811bd4f09e19b6469b73a86be2b79b272e755d0f
Jay Patel <0807Jpatel@gmail.com>
2017-01-28 10:36:06 -0500

    more error fixing

--- index.js
+++ index.js
@@ -63,8 +63,7 @@ function sendText(sender, text){
 // })
 
 app.post('/packageUpdate/', function(req, res){
-    sendText( 1386905277995957 , "something updated");
-    console.log(req.body);
+    sendText( 1386905277995957 , req.body.tracking_number);
     res.sendStatus(200);
 });
 
